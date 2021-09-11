42444db64e8b17617b9e96347be69119dc0b3ba5
Ubuntu <ubuntu@ip-172-31-21-204.ec2.internal>
2021-09-11 03:49:42 +0000

    proper format

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Lily Liu All rights reserved
+//
+// Created by: Lily Liu
+// Created on: Sept 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!";
+}
